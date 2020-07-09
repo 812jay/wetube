fb7f47b51b529e3cfd59820ed6cc11b5480f9ca0
812jay <yongje812@gmail.com>
2020-07-09 23:15:26 +0900

    2.11 MVC Pattern part Three

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 //Global
 const HOME = "/";
-const JOIN = "join";
+const JOIN = "/join";
 const LOGIN = "/login";
 const LOGOUT = "/logout";
 const SEARCH = "/search";
@@ -8,7 +8,7 @@ const SEARCH = "/search";
 //Users
 const USERS = "/users";
 const USER_DETAIL = "/:id";
-const EDIT_PROFILE = "edit-profile";
+const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
 
 //Videos
@@ -16,7 +16,7 @@ const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
-const DELETE_VIDEO = "/:id/delte";
+const DELETE_VIDEO = "/:id/delete";
 
 const routes = {
   home: HOME,
