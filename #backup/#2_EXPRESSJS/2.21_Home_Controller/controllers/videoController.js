47db4057bd0c2f057da460bfa792728805757260
812jay <yongje812@gmail.com>
2020-07-21 00:18:45 +0900

    2.21 Home Controller

---
+++ #backup/#2_EXPRESSJS/2.21_Home_Controller/controllers/videoController.js
@@ -0,0 +1,21 @@
+import { videos } from "../db";
+//template, template에 추가 할 정보가 담긴 객체
+//videos: videos --> videos
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
+export const search = (req, res) => {
+  // const searchingBy = req.query.term;
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) =>
+  res.render("editvideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("delteVideo", { pageTitle: "Delete Video" });
