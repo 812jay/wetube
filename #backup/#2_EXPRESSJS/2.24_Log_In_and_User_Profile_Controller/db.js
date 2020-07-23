ee59b9834da15e0260ca244a59f58a8ae94c9fb2
812jay <yongje812@gmail.com>
2020-07-23 17:48:04 +0900

    2.24 Log In and User Profile Controller

---
+++ #backup/#2_EXPRESSJS/2.24_Log_In_and_User_Profile_Controller/db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is somthig I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 1212121,
+    title: "Video Nice",
+    description: "This is somthig I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 4545445,
+    title: "Video Dope",
+    description: "This is somthig I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 7878787,
+    title: "Video incredible",
+    description: "This is somthig I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+];
