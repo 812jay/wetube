6a8a4c7f967f2ee268cb3a6997624fad0b4de3f0
812jay <yongje812@gmail.com>
2020-07-31 10:54:44 +0900

    3.11 Installing ESLint

---
+++ #backup/#3 MONGODB/3.11_Installing_ESLint/.eslintrc.js
@@ -0,0 +1,33 @@
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es2020": true
+//     },
+//     "extends": "eslint:recommended",
+//     "parserOptions": {
+//         "ecmaVersion": 11,
+//         "sourceType": "module"
+//     },
+//     "rules": {
+//     }
+// };
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
