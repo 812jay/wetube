2659f68cf684bbe3c1e8f8551c10e622902b192e
812jay <yongje812@gmail.com>
2020-07-31 10:57:14 +0900

    3.11 Installing ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,3 @@
-// module.exports = {
-//     "env": {
-//         "browser": true,
-//         "es2020": true
-//     },
-//     "extends": "eslint:recommended",
-//     "parserOptions": {
-//         "ecmaVersion": 11,
-//         "sourceType": "module"
-//     },
-//     "rules": {
-//     }
-// };
 module.exports = {
   env: {
     browser: true,
