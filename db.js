f6c0863cb15afccf360b7b312f7a987dbdf3838e
812jay <yongje812@gmail.com>
2020-11-01 15:58:48 +0900

    11.4 MongoLab is Dead Update

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 //여기서 우리한테 요청하는건 String으로 된 DB
 //어디에 DB가 저장되어 있는지 알려줌
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL_PROD, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
@@ -13,8 +14,9 @@ mongoose.connect(process.env.MONGO_URL, {
 //mongoDB와의 연결
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅ Connected to DB!!");
-const handleError = () => console.log(`❌ Error on DB Connection:${error}`);
-//once : 한번 실행 되는 것.
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
+
 db.once("open", handleOpen);
 db.on("error", handleError);
